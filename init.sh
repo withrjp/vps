89bef6024d58306248e0d0cbe286688ffc5631a6
饶京鹏 <jp.rao@tianrang-inc.com>
2021-11-11 11:37:23 +0800

    [!] fix mysql can't mount

--- init.sh
+++ init.sh
@@ -23,7 +23,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update && apt-get install docker-ce
+apt-get update && apt-get install -y docker-ce
 
 docker --version
 
